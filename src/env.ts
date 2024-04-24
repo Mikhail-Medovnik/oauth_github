88463039ea1736cf8354691f1b55f435c1fc8a1c
Mikhail Medovnik <mmedovnik@aligntech.com>
2024-04-24 13:56:17 +0300

    wip

--- src/env.ts
+++ src/env.ts
@@ -1,10 +1,12 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 const envSchema = z.object({
   PORT: z.preprocess(Number, z.number()).default(3000),
-  HOST: z.string().default('localhost'),
-  NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
-  CORS: z.string().default(''),
+  HOST: z.string().default("localhost"),
+  NODE_ENV: z.enum(["development", "production", "test"]).default("production"),
+  CORS: z.string().default(""),
+  CLIENT_SECRET: z.string().default("10efac93bba8e282f1a2d0aa0b983fa32125a511"),
+  CLIENT_ID: z.string().default("Iv1.73549745fd0ce6e9"),
 });
 
 export const env = envSchema.parse(process.env);
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import cors from '@fastify/cors';
-import formBody from '@fastify/formbody';
-import helmet from '@fastify/helmet';
-import fastify from 'fastify';
-import { env } from './env.js';
-import { getLogger } from './logger.js';
+import cors from "@fastify/cors";
+import formBody from "@fastify/formbody";
+import helmet from "@fastify/helmet";
+import fastify from "fastify";
+import { env } from "./env.js";
+import { getLogger } from "./logger.js";
 
 const server = fastify({
   logger: getLogger(),
@@ -14,12 +14,14 @@ const server = fastify({
 server.register(helmet);
 server.register(formBody);
 
-if (env.CORS !== '') {
-  server.register(cors, { origin: env.CORS.split(',') });
+if (env.CORS !== "") {
+  server.register(cors, { origin: env.CORS.split(",") });
 }
 
-server.get('/api/healthcheck', async () => {
-  return { ok: true };
+server.get("/api/healthcheck", async () => {
+  const secret = env.CLIENT_SECRET;
+  const id = env.CLIENT_ID;
+  return { ok: "works", secret, id };
 });
 
 export { server };
